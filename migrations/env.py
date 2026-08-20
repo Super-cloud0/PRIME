010cf9037c778cad4daaf3b1885980d5cda59a27
Super-cloud0 <brobropups@gmail.com>
2026-08-20 12:36:31 +0500

    Configure Alembic migration environment

---
+++ migrations/env.py
@@ -0,0 +1,35 @@
+from logging.config import fileConfig
+
+from alembic import context
+from flask import current_app
+from sqlalchemy import engine_from_config, pool
+
+config = context.config
+if config.config_file_name:
+    fileConfig(config.config_file_name)
+
+target_metadata = current_app.extensions["migrate"].db.metadata
+
+
+def get_url():
+    return current_app.extensions["migrate"].db.engine.url.render_as_string(hide_password=False)
+
+
+def run_migrations_offline():
+    context.configure(url=get_url(), target_metadata=target_metadata, literal_binds=True, dialect_opts={"paramstyle": "named"})
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+def run_migrations_online():
+    connectable = current_app.extensions["migrate"].db.engine
+    with connectable.connect() as connection:
+        context.configure(connection=connection, target_metadata=target_metadata)
+        with context.begin_transaction():
+            context.run_migrations()
+
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
