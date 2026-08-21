6270c67c2e8492c12ccc98e3a888a2b2a9736910
Super-cloud0 <brobropups@gmail.com>
2026-08-21 17:07:25 +0500

    fix: prevent Telegram auth requests from hanging forever

---
+++ fetch_guard.js
@@ -0,0 +1,13 @@
+(() => {
+  const nativeFetch = window.fetch.bind(window);
+  window.fetch = (input, init = {}) => {
+    const controller = new AbortController();
+    const callerSignal = init.signal;
+    if (callerSignal) {
+      if (callerSignal.aborted) controller.abort();
+      else callerSignal.addEventListener('abort', () => controller.abort(), { once: true });
+    }
+    const timeout = setTimeout(() => controller.abort(), 15000);
+    return nativeFetch(input, { ...init, signal: controller.signal }).finally(() => clearTimeout(timeout));
+  };
+})();
